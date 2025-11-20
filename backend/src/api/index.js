5b1ecde70d9e8576b6d1561187fd751ced3882e2
xavierjosefs <xavierfernandezsantana.com>
2025-11-20 01:29:32 -0400

    Feature: Registro usuarios cliente

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,9 +1,11 @@
 // src/api/index.js
 import express from "express";
+import authRoutes from "./auth.routes.js";
 
 const router = express.Router();
 // aquí van más rutas luego
 
+router.use("/auth", authRoutes);
 
 
 export default router;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,9 @@
 import express from "express";
-import apiRouter from "./api/index.js"; // las rutas
+import apiRouter from "./api/index.js";
 
 const app = express();
 
 app.use(express.json());
-
-// prefijo para rutas
 app.use("/api", apiRouter);
 
 export default app;
