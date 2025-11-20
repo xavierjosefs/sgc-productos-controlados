fbbec3310fcabed94a0f34a9e8b21d38d90e3bba
xavierjosefs <xavierfernandezsantana.com>
2025-11-20 12:51:19 -0400

    fix: correo para el proyecto

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listen port ${PORT}`);
