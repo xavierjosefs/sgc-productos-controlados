6d388d67b186ca0241449c95a01a22d5d720ca5e
xavierjosefs <xavierfernandezsantana@gmail.com>
2025-11-26 17:07:36 -0400

    Add: falta probar la subida de documento

--- backend/src/api/request.routes.js
+++ backend/src/api/request.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { upload } from "../middleware/upload.middleware.js";
-import { createRequestController, getRequestsController, uploadDocumentController } from "../controllers/request.controllers.js";
+import {
+  createRequestController,
+  getRequestsController,
+  uploadDocumentController,
+} from "../controllers/request.controllers.js";
 
 const router = express.Router();
 router.post("/create-requests", createRequestController);
 router.get("/get-requests", getRequestsController);
-router.post("/:id/document", upload.single("document"), uploadDocumentController);
+router.post(
+  "/:id/document",
+  upload.single("document"),
+  uploadDocumentController
+);
 
 export default router;
