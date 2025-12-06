aca76eeefe0f9c1730a28af0f4dc826aace1d77b
lJorge23 <1122909@est.intec.edu.do>
2025-12-06 15:14:51 -0400

    no se que paso

--- frontend/src/pages/Requests.jsx
+++ frontend/src/pages/Requests.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ClientTopbar from '../components/ClientTopbar';
 import useRequestsAPI from '../hooks/useRequestsAPI';
--- frontend/src/pages/SolicitudEnviadaExito.jsx
+++ frontend/src/pages/SolicitudEnviadaExito.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+﻿import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SolicitudEnviadaExito = () => {
